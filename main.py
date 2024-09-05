249bd4ca3a70d574561c9d18c58d576a228df6fc
JESULINJIMENEZ <jesusdavidjc07@gmail.com>
2024-09-05 09:33:00 -0500

    Cambios con la api funcional

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+from fastapi import FastAPI, HTTPException, Depends
+import uvicorn
+
 from api.routes.authenticate import authenticate_user
 from api.routes.register import register_user
+from typing import Optional
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenido a la API"}
+
+@app.post("/auth/token")
+async def authenticate_user(token: Optional[str] = None):
+    await authenticate_user(token)
+
+@app.post("/register/")
+async def register_user(name: str):
+    await register_user(name)
 
 if __name__ == "__main__":
-    option = input("¿Deseas (r)egistrar un nuevo usuario o (a)utenticar un usuario? [r/a]: ")
-    
-    if option.lower() == 'r':
-        name = input("Introduce el nombre del usuario: ")
-        register_user(name)
-    elif option.lower() == 'a':
-        authenticate_user()
-    else:
-        print("Opción no válida.")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
